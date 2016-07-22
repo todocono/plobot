4fec0937bf7c9b44ab0e0e17bac89a6ddfab6c28
Sean Purser-Haskell <sean.purserhaskell@gmail.com>
2016-07-22 11:42:26 +0800

    - Sound fix

--- Main_19JUL2016/sound.cpp
+++ Main_19JUL2016/sound.cpp
@@ -16,15 +16,12 @@ void chip_tone(unsigned long for_millis, unsigned long period_micros, int volume
     digitalWrite(pin_spk_dir, HIGH);
     analogWrite(pin_spk_en, volume);
     delayMicroseconds(period_micros / 2);
-    analogWrite(pin_spk_en, 0);
-    delayMicroseconds(10);
 
     digitalWrite(pin_spk_dir, LOW);
     analogWrite(pin_spk_en, volume);
     delayMicroseconds(period_micros / 2);
-    analogWrite(pin_spk_en, 0);
-    delayMicroseconds(10);
   }
+  analogWrite(pin_spk_en, 0);
 }
 
 
--- Main_19JUL2016/sound.h
+++ Main_19JUL2016/sound.h
@@ -2,7 +2,7 @@
 #ifndef SOUND_H
 #define SOUND_H
 
-#define DEFAULT_VOLUME 0 // 75
+#define DEFAULT_VOLUME 70
 
 void init_sound();
 void chip_tone(unsigned long for_millis, unsigned long period_micros, int volume=DEFAULT_VOLUME);
